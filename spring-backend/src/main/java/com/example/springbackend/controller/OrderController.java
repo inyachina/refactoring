753aa5f57bbe4670d170cee08e7ecfb6c0c552f3
inyachina <dianainyachina@gmail.com>
2022-12-19 19:41:07 +0300

    remove try-catch from controllers + add throwing exceptions

--- spring-backend/src/main/java/com/example/springbackend/controller/OrderController.java
+++ spring-backend/src/main/java/com/example/springbackend/controller/OrderController.java
@@ -1,7 +1,6 @@
 package com.example.springbackend.controller;
 
 import com.example.springbackend.data.dto.ProductOrderDTO;
-import com.example.springbackend.facade.ErrorBody;
 import com.example.springbackend.facade.Response;
 import com.example.springbackend.model.Human;
 import com.example.springbackend.model.HumanOrder;
@@ -9,12 +8,10 @@ import com.example.springbackend.model.Product;
 import com.example.springbackend.model.ProductOrder;
 import com.example.springbackend.service.impl.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -42,118 +39,69 @@ public class OrderController {
 
     @PostMapping("/human-fate/{id}")
     public ResponseEntity<Response<Object>> saveHumanFateOrder(@PathVariable Integer id, HttpServletRequest req) {
-        try {
-            this.humanOrderService.save(new HumanOrder(
-                    "IN_PROGRESS",
-                    this.userService.findByLogin(req.getHeader("login")).getId(),
-                    this.humanService.findById(id).get()));
-            return Response.success();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return Response.failure(
-                    Response.error(
-                            new ErrorBody(e.getMessage(),
-                                    HttpStatus.INTERNAL_SERVER_ERROR, Arrays.toString(e.getStackTrace()))));
-        }
+        this.humanOrderService.save(new HumanOrder(
+                "IN_PROGRESS",
+                this.userService.findByLogin(req.getHeader("login")).getId(),
+                this.humanService.findById(id).get()));
+        return Response.success();
     }
 
     @GetMapping("/human-fate/{id}")
     public ResponseEntity<Response<Object>> findHumanFateOrder(@PathVariable Integer id) {
-        try {
-            Optional<HumanOrder> humanOrder = this.humanOrderService.findByHuman_Id(id);
-            if (humanOrder.isPresent()) return Response.success(humanOrder);
-            else return Response.success(null);
-        } catch (Exception e) {
-            return Response.failure(
-                    Response.error(
-                            new ErrorBody(e.getMessage(),
-                                    HttpStatus.INTERNAL_SERVER_ERROR, Arrays.toString(e.getStackTrace()))));
-        }
+        Optional<HumanOrder> humanOrder = this.humanOrderService.findByHuman_Id(id);
+        if (humanOrder.isPresent()) return Response.success(humanOrder);
+        else return Response.success(null);
     }
 
     @GetMapping("/human-fate/active")
     public ResponseEntity<Response<Object>> findAllActiveHumanOrders() {
-        try {
-            List<HumanOrder> humanOrders = this.humanOrderService.findAllByStatus("IN_PROGRESS");
-            return Response.success(this.humanService.findAllById(humanOrders.stream().map(HumanOrder::getId).toList()));
-        } catch (Exception e) {
-            return Response.failure(
-                    Response.error(
-                            new ErrorBody(e.getMessage(),
-                                    HttpStatus.INTERNAL_SERVER_ERROR, Arrays.toString(e.getStackTrace()))));
-        }
+        List<HumanOrder> humanOrders = this.humanOrderService.findAllByStatus("IN_PROGRESS");
+        return Response.success(this.humanService.findAllById(humanOrders.stream().map(HumanOrder::getId).toList()));
     }
 
     @PutMapping("/human-fate/accept/{id}")
     public ResponseEntity<Response<Object>> acceptHumanFateOrder(@PathVariable Integer id, @RequestBody String fate) {
-        try {
-            HumanOrder humanOrder = this.humanOrderService.findByHuman_Id(id).get();
-            humanOrder.setStatus("ACCEPTED");
-            Human human = this.humanService.findById(id).get();
-            human.setFate(fate);
-            this.humanService.save(human);
-            return Response.success();
-        } catch (Exception e) {
-            return Response.failure(
-                    Response.error(
-                            new ErrorBody(e.getMessage(),
-                                    HttpStatus.INTERNAL_SERVER_ERROR, Arrays.toString(e.getStackTrace()))));
-        }
+        HumanOrder humanOrder = this.humanOrderService.findByHuman_Id(id).get();
+        humanOrder.setStatus("ACCEPTED");
+        Human human = this.humanService.findById(id).get();
+        human.setFate(fate);
+        this.humanService.save(human);
+        return Response.success();
     }
 
     @PostMapping("/product")
     public ResponseEntity<Response<Object>> saveProductOrder(@RequestBody ProductOrderDTO dao, HttpServletRequest req) {
-        try {
-            this.productOrderService.save(new ProductOrder(
-                    "IN_THE_BASKET",
-                    this.userService.findByLogin(req.getHeader("login")).getId(),
-                    dao.getProductId(),
-                    this.userService.findByLogin(dao.getToUser()).getId(),
-                    dao.getFromTime(),
-                    dao.getToTime()));
+        this.productOrderService.save(new ProductOrder(
+                "IN_THE_BASKET",
+                this.userService.findByLogin(req.getHeader("login")).getId(),
+                dao.getProductId(),
+                this.userService.findByLogin(dao.getToUser()).getId(),
+                dao.getFromTime(),
+                dao.getToTime()));
 
-            Product product = this.productService.findById(dao.getProductId()).get();
-            product.setSold(true);
-            this.productService.save(product);
-            return Response.success();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return Response.failure(
-                    Response.error(
-                            new ErrorBody(e.getMessage(),
-                                    HttpStatus.INTERNAL_SERVER_ERROR, Arrays.toString(e.getStackTrace()))));
-        }
+        Product product = this.productService.findById(dao.getProductId()).get();
+        product.setSold(true);
+        this.productService.save(product);
+        return Response.success();
     }
 
     @GetMapping("/product")
     public ResponseEntity<Response<Object>> findAllProductOrders(HttpServletRequest request) {
-        try {
-            int id = this.userService.findByLogin(request.getHeader("login")).getId();
-            List<ProductOrder> orders = this.productOrderService.findAllByStatus("IN_THE_BASKET")
-                    .stream().filter(productOrder -> productOrder.getFromUser() == id).toList();
-            return Response.success(this.productService.findAllById(orders.stream().map(ProductOrder::getProductId).toList()));
-        } catch (Exception e) {
-            return Response.failure(
-                    Response.error(
-                            new ErrorBody(e.getMessage(),
-                                    HttpStatus.INTERNAL_SERVER_ERROR, Arrays.toString(e.getStackTrace()))));
-        }
+        int id = this.userService.findByLogin(request.getHeader("login")).getId();
+        List<ProductOrder> orders = this.productOrderService.findAllByStatus("IN_THE_BASKET")
+                .stream().filter(productOrder -> productOrder.getFromUser() == id).toList();
+        return Response.success(this.productService.findAllById(orders.stream().map(ProductOrder::getProductId).toList()));
+
     }
 
     @DeleteMapping("/product/basket/{orderId}")
     public ResponseEntity<Response<Object>> deleteProductOrder(@PathVariable Integer orderId, HttpServletRequest req) {
-        try {
-            int productId = this.productOrderService.findById(orderId).get().getProductId();
-            this.productOrderService.deleteById(orderId);
-            Product product = this.productService.findById(productId).get();
-            product.setSold(false);
-            this.productService.save(product);
-            return Response.success();
-        } catch (Exception e) {
-            return Response.failure(
-                    Response.error(
-                            new ErrorBody(e.getMessage(),
-                                    HttpStatus.INTERNAL_SERVER_ERROR, Arrays.toString(e.getStackTrace()))));
-        }
+        int productId = this.productOrderService.findById(orderId).get().getProductId();
+        this.productOrderService.deleteById(orderId);
+        Product product = this.productService.findById(productId).get();
+        product.setSold(false);
+        this.productService.save(product);
+        return Response.success();
+
     }
 }
