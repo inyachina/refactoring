92c6bc3ed4aebfe554dde4025ad9a8d56168fe32
inyachina <dianainyachina@gmail.com>
2022-12-19 19:32:59 +0300

    add exceptions with http status and default description

---
+++ spring-backend/src/main/java/com/example/springbackend/exception/NotFoundException.java
@@ -0,0 +1,18 @@
+package com.example.springbackend.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class NotFoundException extends ApiException {
+
+    public NotFoundException() {
+        super(HttpStatus.NOT_FOUND, "Not Found");
+    }
+
+    public NotFoundException(String message) {
+        this();
+        super.message = message;
+    }
+
+}
